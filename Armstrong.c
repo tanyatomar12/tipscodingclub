76d080dc0a6c154805d2089f537c8a147c628287
tanyatomar12 <122167775+tanyatomar12@users.noreply.github.com>
2023-01-08 10:25:03 +0530

    Create Armstrong.c

---
+++ Armstrong.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int num, onum, rem, res=0;
+    printf("Enter a three digit integer=");
+    scanf("%d",&num);
+    onum=num;
+
+    while(onum!=0)
+    {
+        rem=onum%10;
+        res=res+(rem*rem*rem);
+        onum=onum/10;
+    }
+
+    if (res==num)
+    printf("%d is an Armstrong number",num);
+    else
+    printf("%d is not a Armstrong number",num);
+
+    return 0;
+}
